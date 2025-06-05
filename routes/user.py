bb1b5fcb214a53a74a30445e528d7d3fdb4d5b68
oscurantismo <lubyatowo@gmail.com>
2025-06-05 00:32:34 -0400

    Update user.py

--- routes/user.py
+++ routes/user.py
@@ -1,7 +1,9 @@
-from flask import request, jsonify
-from handling.user_coins import get_user_coins
+from flask import Blueprint, request, jsonify
+from handling.coin_rewards import get_user_coins
 
-@app.route("/api/user-coins", methods=["GET"])
+user = Blueprint("user", __name__)
+
+@user.route("/api/user-coins", methods=["GET"])
 def fetch_user_coins():
     user_id = request.args.get("id")
     if not user_id:
